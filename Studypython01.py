65c38ac9fcaeac04836f608a2c7a35c5ca917676
Dan Browly <61484204+DanBrowly@users.noreply.github.com>
2022-09-06 23:53:25 -0300

    Create Studypython01.py

---
+++ Studypython01.py
@@ -0,0 +1,23 @@
+#if the number is equal, more or less 
+
+x = int(input('Digite um número para equiparar a 5 '))
+
+if (x >= 11):
+  print('Q issu qr derruba o avião??')
+elif(x < 5):
+  print(x, 'é Menor')
+elif (x > 5):
+  print(x, 'é Maior')
+else:
+  print(x, 'é Igual')
+  
+#-----------------
+idk thi code
+
+a = 30
+b = 20
+print(a, b)
+a = a + b
+b = a - b
+a = a - b
+print(a, b)
